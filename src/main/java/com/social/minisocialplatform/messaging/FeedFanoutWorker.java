b2df6f52e74fd913c2d98308f5de60fc237d1cad
harshavardhan-9 <vardhanharsha607@gmail.com>
2026-05-23 17:54:32 +0530

    implemented backpressure handling with bounded queue simulation

--- src/main/java/com/social/minisocialplatform/messaging/FeedFanoutWorker.java
+++ src/main/java/com/social/minisocialplatform/messaging/FeedFanoutWorker.java
@@ -8,19 +8,41 @@ public class FeedFanoutWorker {
         
     private IdempotencyStore idempotencyStore;
 
+    private static int pendingEvents = 0;
+
     public FeedFanoutWorker(IdempotencyStore idempotencyStore) {
         this.idempotencyStore = idempotencyStore;
     }
 
     @RabbitListener(queues = RabbitMQConfig.FEED_QUEUE)
     public void handlePostCreatedEvent(PostCreatedEvent event) {
+        pendingEvents++;
+        System.out.println("Current pending events in feed worker: " + pendingEvents);
+        
+        if(pendingEvents > 5) {
+            System.out.println("Backpressure triggered. Dropping event: " + event.getEventId());
+            pendingEvents--;
+            return;
+        }
+
         if (idempotencyStore.isFeedEventProcessed(event.getEventId())) {
             System.out.println("Duplicate event skipped in feed worker: " + event.getEventId());
             return;
         }
-        System.out.println("Feed worker processing event: " + event.getEventId());
-        System.out.println("Updating feed for user: " + event.getUserId());
+        
+        new Thread(() -> {
+            System.out.println("Feed worker processing event: " + event.getEventId());
+            System.out.println("Updating feed for user: " + event.getUserId());
 
-        idempotencyStore.markFeedEventProcessed(event.getEventId());
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            idempotencyStore.markFeedEventProcessed(event.getEventId());
+            pendingEvents--;
+
+        }).start();
     }
 }
